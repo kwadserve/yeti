18318dac8f6e0ca2f112ea1e147b174b07925fc4
Rakesh <rakeshranjanjena7@gmail.com>
2023-08-25 00:33:12 +0530

    Bug Fixes

--- resources/views/frontend/interview.blade.php
+++ resources/views/frontend/interview.blade.php
@@ -10,27 +10,27 @@
                 <div class="col-lg-3"></div>
                 <div class="col-lg-6">
                     <!-- Interview Date Selected! Waiting for admin approval... -->
-                    @if ($status === 'pending')
+                    @if ($status->meta_value === 'pending')
                         <h3 class="title">Interview Booked!</h3>
                         <p>Thank you <strong>{{ Auth::user()->name }}</strong> for booking an interview on
                             {{ $date }} at 12:00 noon. We will email you regarding your status..</p>
                     <!-- Interview Date accepted by admin... -->
-                    @elseif($status === 'booked')
+                    @elseif($status->meta_value === 'booked')
                         <h3 class="title">Interview Accepted!</h3>
                         <p><strong>{{ Auth::user()->name }}</strong> your interview on
                             {{ $date }} at 12:00 noon has been accepted. We have send you an email with the link to
                             the meeting..</p>
                     <!-- Interview Date rejected by admin... -->
-                    @elseif($status === 'declined')
+                    @elseif($status->meta_value === 'declined')
                         <h3 class="title">Interview Declined!</h3>
                         <p><strong>{{ Auth::user()->name }}</strong> your interview on
                             {{ $date }} at 12:00 noon has been rejected. An email has been sent stating the rejection reason.</p>
                     <!-- User rejected as YETI by admin... -->
-                    @elseif($status === 'rejected')
+                    @elseif($status->meta_value === 'rejected')
                         <h3 class="title">Sorry <strong>{{ Auth::user()->name }}</strong>!</h3>
                         <p>You are not accepted as an YETI.</p>
                     <!-- User accepted as YETI by admin... -->
-                    @elseif($status === 'accepted')
+                    @elseif($status->meta_value === 'accepted')
                         <h3 class="title">Congratulation! <strong>{{ Auth::user()->name }}</strong></h3>
                         <p>Your interview on
                             {{ $date }} was successful. You have been accepted as an YETI. Please click here to <a
